94b75b15435d036a8c4721fdca587572a693ba22
CrazyMerlyn <ankitgoel616@gmail.com>
2014-10-30 15:14:27 -0400

    Corrected screen dimensions

--- game/main.js
+++ game/main.js
@@ -2,7 +2,7 @@
 
 //global variables
 window.onload = function () {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'flappy-bird');
+  var game = new Phaser.Game(288, 505, Phaser.AUTO, 'flappy-bird');
 
   // Game States
   game.state.add('boot', require('./states/boot'));

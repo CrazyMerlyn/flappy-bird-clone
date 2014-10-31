139c8dbdb4d806fef9e25b1c8af1729e50976cdc
CrazyMerlyn <ankitgoel616@gmail.com>
2014-10-31 00:03:19 -0400

    Changed preloader gif position

--- game/states/preload.js
+++ game/states/preload.js
@@ -7,7 +7,7 @@ function Preload() {
 
 Preload.prototype = {
     preload: function() {
-        this.asset = this.add.sprite(this.width / 2, this.height / 2, 'preloader');
+        this.asset = this.add.sprite(this.game.width/2, this.game.height / 2, 'preloader');
         this.asset.anchor.setTo(0.5, 0.5);
         
         // Set the preloader gif
